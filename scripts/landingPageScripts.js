f145ac4979a8441f0d4008d595d21ff6d4e1c324
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-08-13 11:03:07 -0300

    Porcaria de LandingPage

---
+++ scripts/landingPageScripts.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('#mySidenav [data-target]').forEach(option => {
+    option.addEventListener('click', e => {
+        e.preventDefault();
+
+        const optionID = option.getAttribute('data-target');
+        const optionElement = document.getElementById(optionID);
+
+        if (optionElement) {
+            optionElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
--- scriptsGlobal.js
+++ scriptsGlobal.js
@@ -1,7 +1,43 @@
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+function plusSlides(x) {
+    showSlides(slideIndex += x);
+}
+
+function currentSlide(x) {
+    showSlides(slideIndex = x);
+}
+
+function showSlides(x) {
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (x > slides.length) {
+        slideIndex = 1;
+    }
+    else if (x < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    dots[slideIndex - 1].className += " active";
 }
